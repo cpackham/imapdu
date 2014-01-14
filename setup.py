f5ea7602c147dfc930596be38f0d3b5ff306d7a5
Chris Packham <judge.packham@gmail.com>
2014-01-14 15:53:17 +1300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+setup(name='imapdu',
+      version='1.0',
+      description='Disk usage calculator for IMAP',
+      author='Chris Packham',
+      scripts=['imapdu'])
